eb5090d41604ab03ba91ef939cdf4009d3391593
Artur Szałata <arturs6668@gmail.com>
2021-06-15 16:33:49 +0200

    finalize landscape visualization

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import copy
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+
 matplotlib.rcParams['figure.figsize'] = [18, 12]
 import loss_landscapes
 import loss_landscapes.metrics
@@ -79,6 +80,7 @@ device = torch.device("cuda" if args.cuda else "cpu")
 ###############################################################################
 dataset_train = SeqDataset(args.data_dir, "train")
 dataset_val = SeqDataset(args.data_dir, "val")
+dataset_test = SeqDataset(args.data_dir, "test")
 dataloader_train = DataLoader(dataset_train,
                               shuffle=True,
                               collate_fn=dataset_train.collate_fn,
@@ -87,6 +89,10 @@ dataloader_val = DataLoader(dataset_val,
                             shuffle=False,
                             collate_fn=dataset_val.collate_fn,
                             batch_size=args.batch_size)
+dataloader_test = DataLoader(dataset_test,
+                             shuffle=False,
+                             collate_fn=dataset_test.collate_fn,
+                             batch_size=args.batch_size)
 
 if args.model == 'Transformer':
     model = model.TransformerModel(args.emsize, args.nhead, args.nhid, args.nlayers, args.task,
@@ -97,7 +103,6 @@ else:
 
 # for loss visualization
 model_initial = copy.deepcopy(model)
-
 
 print(f"{model.count_parameters()} parameters")
 
@@ -140,39 +145,52 @@ def evaluate(dataloader):
     return loss_per_sample, total_accuracy
 
 
-def loss_landscape_viz(model_initial, model_final, criterion, loader, save_dir):
+def loss_landscape_viz(model_initial, model_final, criterion, loader_train, loader_test, save_dir):
     STEPS = 100
     MAX_DIST = 100
-
     CONTOUR_LEVELS = 50
-    x, y, mask = iter(loader).__next__()
+
+    x, y, mask = iter(loader_train).__next__()
     metric = loss_landscapes.metrics.Loss(criterion, x, y, mask)
+
+    x, y, mask = iter(loader_test).__next__()
+    metric_test = loss_landscapes.metrics.Loss(criterion, x, y, mask)
 
     # interpolation
     loss_data = loss_landscapes.linear_interpolation(model_initial, model_final, metric, STEPS,
                                                      deepcopy_model=True)
-    plt.plot([1/STEPS * i for i in range(STEPS)], loss_data)
+    loss_data_test = loss_landscapes.linear_interpolation(model_initial, model_final, metric_test,
+                                                          STEPS, deepcopy_model=True)
+    plt.plot([1 / STEPS * i for i in range(STEPS)], loss_data, label="train")
+    plt.plot([1 / STEPS * i for i in range(STEPS)], loss_data_test, label="test")
     plt.xlabel('Interpolation Coefficient')
     plt.title("Linear interpolation of loss")
     plt.ylabel('Loss')
-    plt.savefig(os.path.join(save_dir, "loss_interpolation.jpg"))
+    plt.legend()
+    plt.savefig(os.path.join(save_dir, "loss_interpolation.pdf"))
 
-    # contour
-    plt.figure()
-    loss_data_fin = loss_landscapes.random_plane(model_final, metric, MAX_DIST, STEPS,
-                                                 normalization='filter', deepcopy_model=True)
-    plt.contour(loss_data_fin, levels=CONTOUR_LEVELS)
-    plt.title('Loss Contours around Trained Model')
-    plt.savefig(os.path.join(save_dir, "loss_contour_around_final.jpg"))
+    STEPS = 50
 
-    # 3d landscape
-    plt.figure()
-    ax = plt.axes(projection='3d')
-    X = np.array([[j for j in range(STEPS)] for i in range(STEPS)])
-    Y = np.array([[i for _ in range(STEPS)] for i in range(STEPS)])
-    ax.plot_surface(X, Y, loss_data_fin, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
-    ax.set_title('Surface Plot of Loss Landscape')
-    plt.savefig(os.path.join(save_dir, "loss_3d_around_final.jpg"))
+    def draw_contour_plots(metric_contour, split):
+        # contour
+        plt.figure()
+        loss_data_fin = loss_landscapes.random_plane(model_final, metric_contour, MAX_DIST, STEPS,
+                                                     normalization='layer', deepcopy_model=True)
+        plt.contour(loss_data_fin, levels=CONTOUR_LEVELS)
+        plt.title('Loss Contours around Trained Model')
+        plt.savefig(os.path.join(save_dir, f"loss_contour_around_final_{split}.pdf"))
+
+        # 3d landscape
+        plt.figure()
+        ax = plt.axes(projection='3d')
+        X = np.array([[j for j in range(STEPS)] for i in range(STEPS)])
+        Y = np.array([[i for _ in range(STEPS)] for i in range(STEPS)])
+        ax.plot_surface(X, Y, loss_data_fin, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+        ax.set_title('Surface Plot of Loss Landscape')
+        plt.savefig(os.path.join(save_dir, f"loss_3d_around_final_{split}.pdf"))
+
+    draw_contour_plots(metric, "train")
+    draw_contour_plots(metric_test, "test")
 
 
 def train():
@@ -183,7 +201,7 @@ def train():
 
     train_iterator = trange(int(args.epochs), desc="Epoch")
     optimizer = AdamW(model.parameters(), lr=args.lr)
-    for e,_ in enumerate(train_iterator):
+    for e, _ in enumerate(train_iterator):
         epoch_iterator = tqdm(dataloader_train)
         for step, batch in enumerate(epoch_iterator):
             data, targets, masks = batch
@@ -203,7 +221,8 @@ def train():
 
             total_loss += loss.item()
 
-            if (step % args.log_interval == 0 and step > 0) or (e==int(args.epochs)-1 and step==len(epoch_iterator)-1):
+            if (step % args.log_interval == 0 and step > 0) or (
+                    e == int(args.epochs) - 1 and step == len(epoch_iterator) - 1):
                 val_loss, val_accuracy = evaluate(dataloader_val)
                 if not best_val_loss or val_loss < best_val_loss:
                     with open(os.path.join(args.save, "model.pt"), 'wb') as f:
@@ -222,5 +241,5 @@ def train():
 best_val_loss = None
 
 final_model = train()
-loss_landscape_viz(model_initial, final_model, criterion, dataloader_train, args.save)
-
+loss_landscape_viz(model_initial, final_model, criterion, dataloader_train, dataloader_test,
+                   args.save)
