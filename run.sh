e3b9fa1ce6409afe19d62d4554c318ddcf1305d9
Artur Szałata <arturs6668@gmail.com>
2021-06-16 13:34:51 +0200

    fix directory naming and lower the sequence length

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 model=${1:-Transformer}
-max_len=${2:-100}
+max_len=${2:-5}
 epochs=${3:-10}
 nlayer=${4:-2}
 nhead=${5:-1}
@@ -27,7 +27,7 @@ do
     echo "sequence classification with fixed length $len"
 
     data_dir="data/sequence_classification/fixed_length"
-    output_dir="output/seq_cls/fixed_length/$model-model_$len-length_$epochs-epochs_$nlayer-layer(s)_$nhead-head(s)_$nhid-hiddendim"
+    output_dir="output/seq_cls/fixed_length/$model-model_$len-length_$epochs-epochs_$nlayer-layer_$nhead-head_$nhid-hiddendim"
     
     python create_sequence_classification_data.py --seq_len $len
     run_experiment $epochs $nhid $model $data_dir $output_dir
@@ -36,7 +36,7 @@ do
     echo "sequence classification with variable length maximum $len"
 
     data_dir="data/sequence_classification/variable_length"
-    output_dir="output/seq_cls/variable_length/$model-model_$len-length_$epochs-epochs_$nlayer-layer(s)_$nhead-head(s)_$nhid-hiddendim"
+    output_dir="output/seq_cls/variable_length/$model-model_$len-length_$epochs-epochs_$nlayer-layer_$nhead-head_$nhid-hiddendim"
     
     python create_sequence_classification_data.py --seq_len $len --variable_length
     run_experiment$epochs $nhid $model $data_dir $output_dir
@@ -48,7 +48,7 @@ do
     echo "sequence learning with fixed length $len and fixed increment"
 
     data_dir="data/sequence_learning/fixed_length/fixed_increment"
-    output_dir="output/seq_lrn/fixed_length/fixed_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer(s)_$nhead-head(s)_$nhid-hiddendim"
+    output_dir="output/seq_lrn/fixed_length/fixed_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer_$nhead-head_$nhid-hiddendim"
     
     python create_sequence_learning_data.py --seq_len $len
     run_experiment $epochs $nhid $model $data_dir $output_dir
@@ -57,7 +57,7 @@ do
     echo "sequence learning with fixed length $len  and variable increment"
 
     data_dir="data/sequence_learning/fixed_length/variable_increment"
-    output_dir="output/seq_lrn/fixed_length/var_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer(s)_$nhead-head(s)_$nhid-hiddendim"
+    output_dir="output/seq_lrn/fixed_length/var_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer_$nhead-head_$nhid-hiddendim"
     
     python create_sequence_learning_data.py --seq_len $len --variable_increment
     run_experiment $epochs $nhid $model $data_dir $output_dir
@@ -67,7 +67,7 @@ do
     echo "sequence learning with variable length maximum $len and fixed increment"
     
     data_dir="data/sequence_learning/variable_length/fixed_increment"
-    output_dir="output/seq_lrn/variable_length/fixed_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer(s)_$nhead-head(s)_$nhid-hiddendim"
+    output_dir="output/seq_lrn/variable_length/fixed_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer_$nhead-head_$nhid-hiddendim"
     
     python create_sequence_learning_data.py --seq_len $len --variable_length
     run_experiment $epochs $nhid $model $data_dir $output_dir 
@@ -76,7 +76,7 @@ do
     echo "sequence learning with variable length maximum $len and variable increment"
     
     data_dir="data/sequence_learning/variable_length/variable_increment"
-    output_dir="output/seq_lrn/variable_length/var_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer(s)_$nhead-head(s)_$nhid-hiddendim"
+    output_dir="output/seq_lrn/variable_length/var_inc/$model-model_$len-length_$epochs-epochs_$nlayer-layer_$nhead-head_$nhid-hiddendim"
     
     python create_sequence_learning_data.py --seq_len $len --variable_increment --variable_length
     run_experiment $epochs $nhid $model $data_dir $output_dir 
