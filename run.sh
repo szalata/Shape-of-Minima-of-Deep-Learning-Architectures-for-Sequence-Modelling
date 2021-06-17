a106c249d0fbf555002b58f1b106de02e8f63a63
Artur Szałata <arturs6668@gmail.com>
2021-06-17 09:30:06 +0200

    create different run.sh template

--- run.sh
+++ run.sh
@@ -9,86 +9,111 @@ nhead=${5:-1}
 nhid=${6:-8}
 
 run_experiment() {
-    for ((e=$1;e<=$1*20;e+=50));
+    e=$1
+    for ((hid=$2;hid<=$2*4;hid*=2));
     do
-        for ((hid=$2;hid<=$2*4;hid*=2));
-        do  
-            for ((nl=nlayer;nl<=nlayer+10;nl+=5));
+        for ((nl=nlayer;nl<=nlayer+10;nl+=5));
+        do
+            for ((nh=nhead;nh<=nhead*4;nh*=2));
             do
-                for ((nh=nhead;nh<=nhead*4;nh*=2));
-                do
-                    echo "model: $3, nlayer: $nl, nhid: $hid, nhead: $nh, epochs: $e"
-                    save_dir="$5/$model-model_$len-length_$e-epochs_$nl-layer_$nh-head_$hid-hiddendim"
-                    python main.py --model $3 --nlayers $nl --nhid $hid --nhead $nh --epochs $e --data_dir $4 --save $save_dir
-                done
+                echo "model: $3, nlayer: $nl, nhid: $hid, nhead: $nh, epochs: $e"
+                save_dir="$5/$model-model_$len-length_$e-epochs_$nl-layer_$nh-head_$hid-hiddendim"
+                python main.py --model $3 --nlayers $nl --nhid $hid --nhead $nh --epochs $e --data_dir $4 --save $save_dir
             done
         done
-            
     done
+}
+
+run_single_experiment() {
+    save_dir="$6/$5-model_$7-length_$1-epochs_$3-layer_$4-head_$2-hiddendim"
+    python main.py --model $5 --nlayers $3 --nhid $2 --nhead $4 --epochs $1 --data_dir $8 --save $save_dir
 }
 
 
 for model in "${models[@]}"
 do
+    len=5
+    echo "sequence classification with fixed length $len"
 
-    for ((len=max_len;len<=max_len*25;len*=5));
-    do
-        echo "sequence classification with fixed length $len"
+    data_dir="data/sequence_classification/fixed_length"
+    output_dir="output/seq_cls/fixed_length"
 
-        data_dir="data/sequence_classification/fixed_length"
-        output_dir="output/seq_cls/fixed_length"
+    python create_sequence_classification_data.py --seq_len $len
+    run_experiment $epochs $nhid $model $data_dir $output_dir
 
-        python create_sequence_classification_data.py --seq_len $len
-        run_experiment $epochs $nhid $model $data_dir $output_dir
+    len=5
+    echo "#########################################################"
+    echo "sequence classification with variable length maximum $len"
 
-        echo "#########################################################"
-        echo "sequence classification with variable length maximum $len"
+    data_dir="data/sequence_classification/variable_length"
+    output_dir="output/seq_cls/variable_length"
 
-        data_dir="data/sequence_classification/variable_length"
-        output_dir="output/seq_cls/variable_length"
+    python create_sequence_classification_data.py --seq_len $len --variable_length
 
-        python create_sequence_classification_data.py --seq_len $len --variable_length
-        run_experiment$epochs $nhid $model $data_dir $output_dir
-    done
+    layer=2
+    head=1
+    nhid=8
+    epochs=10
+    run_single_experiment $epochs $nhid $layer $head $model $output_dir $len $data_dir
 
-    for ((len=max_len;len<=max_len*25;len*=5));
-    do
-        echo "#########################################################"
-        echo "sequence learning with fixed length $len and fixed increment"
+    len=5
+    echo "#########################################################"
+    echo "sequence learning with fixed length $len and fixed increment"
 
-        data_dir="data/sequence_learning/fixed_length/fixed_increment"
-        output_dir="output/seq_lrn/fixed_length/fixed_inc"
+    data_dir="data/sequence_learning/fixed_length/fixed_increment"
+    output_dir="output/seq_lrn/fixed_length/fixed_inc"
 
-        python create_sequence_learning_data.py --seq_len $len
-        run_experiment $epochs $nhid $model $data_dir $output_dir
+    python create_sequence_learning_data.py --seq_len $len
 
-        echo "#########################################################"
-        echo "sequence learning with fixed length $len  and variable increment"
+    layer=2
+    head=1
+    nhid=8
+    epochs=10
+    run_single_experiment $epochs $nhid $layer $head $model $output_dir $len $data_dir
 
-        data_dir="data/sequence_learning/fixed_length/variable_increment"
-        output_dir="output/seq_lrn/fixed_length/var_inc"
+    len=5
+    echo "#########################################################"
+    echo "sequence learning with fixed length $len  and variable increment"
 
-        python create_sequence_learning_data.py --seq_len $len --variable_increment
-        run_experiment $epochs $nhid $model $data_dir $output_dir
+    data_dir="data/sequence_learning/fixed_length/variable_increment"
+    output_dir="output/seq_lrn/fixed_length/var_inc"
+
+    python create_sequence_learning_data.py --seq_len $len --variable_increment
+
+    layer=2
+    head=1
+    nhid=8
+    epochs=10
+    run_single_experiment $epochs $nhid $layer $head $model $output_dir $len $data_dir
 
 
-        echo "#########################################################"
-        echo "sequence learning with variable length maximum $len and fixed increment"
+    len=5
+    echo "#########################################################"
+    echo "sequence learning with variable length maximum $len and fixed increment"
 
-        data_dir="data/sequence_learning/variable_length/fixed_increment"
-        output_dir="output/seq_lrn/variable_length/fixed_inc"
+    data_dir="data/sequence_learning/variable_length/fixed_increment"
+    output_dir="output/seq_lrn/variable_length/fixed_inc"
 
-        python create_sequence_learning_data.py --seq_len $len --variable_length
-        run_experiment $epochs $nhid $model $data_dir $output_dir
+    python create_sequence_learning_data.py --seq_len $len --variable_length
 
-        echo "#########################################################"
-        echo "sequence learning with variable length maximum $len and variable increment"
+    layer=2
+    head=1
+    nhid=8
+    epochs=10
+    run_single_experiment $epochs $nhid $layer $head $model $output_dir $len $data_dir
 
-        data_dir="data/sequence_learning/variable_length/variable_increment"
-        output_dir="output/seq_lrn/variable_length/var_inc"
+    len=5
+    echo "#########################################################"
+    echo "sequence learning with variable length maximum $len and variable increment"
 
-        python create_sequence_learning_data.py --seq_len $len --variable_increment --variable_length
-        run_experiment $epochs $nhid $model $data_dir $output_dir
-    done
+    data_dir="data/sequence_learning/variable_length/variable_increment"
+    output_dir="output/seq_lrn/variable_length/var_inc"
 
+    python create_sequence_learning_data.py --seq_len $len --variable_increment --variable_length
+
+    layer=2
+    head=1
+    nhid=8
+    epochs=10
+    run_single_experiment $epochs $nhid $layer $head $model $output_dir $len $data_dir
 done
